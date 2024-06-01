e51461dcdfa8d0b7e3e566a6905bca72d031f3cb
samarthwtf <samarthplayz1337@gmail.com>
2024-06-01 21:39:08 +0530

    arrived till looping

--- main.go
+++ main.go
@@ -7,21 +7,22 @@ func main() {
 	var remainingNiggas uint = 5
 	var CustomerName string
 	var SellingNiggaCount uint
-
-	var nigga_name string
-	var niggas_details [5]string
-
 	fmt.Print("Please Enter your name: ")
 	fmt.Scan(&CustomerName)
 	fmt.Printf("Hey %v Welcome to Nigga Trade Centre\n", CustomerName)
-	fmt.Printf("We have Total: %v Niggers\nAnd Remaining: %v Niggers", TotalAllowedNiggas, remainingNiggas)
+	fmt.Printf("We have Total: %v Niggers\nAnd Remaining: %v Niggers\n", TotalAllowedNiggas, remainingNiggas)
 	fmt.Print("Please Enter the number of Niggas to Book:")
 	fmt.Scan(&SellingNiggaCount)
 	fmt.Printf("%v Booked %v Niggers for their Basement\n", CustomerName, SellingNiggaCount)
 	fmt.Printf("Please Provide the required information ")
-	fmt.Print("Please Enter your nigga's Name: ")
-	fmt.Scan(&nigga_name)
-	niggas_details[0] = nigga_name
-	remainingNiggas = remainingNiggas - SellingNiggaCount
-	fmt.Printf("Remaining Niggas are : %v\n", remainingNiggas)
+	var nigga_name string
+	var niggas_details []string
+	for {
+		fmt.Print("Please Enter your nigga's Name: ")
+		fmt.Scan(&nigga_name)
+		niggas_details = append(niggas_details, nigga_name)
+		fmt.Printf("The nigga's information provided by you is : %v\n", niggas_details)
+		remainingNiggas = remainingNiggas - SellingNiggaCount
+		fmt.Printf("Remaining Niggas are : %v\nThank's for trading Niggers with us", remainingNiggas)
+	}
 }
