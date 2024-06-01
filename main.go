ca545d2a2b88a4279103a8001b0e1d7a67819cee
samarthwtf <samarplayz1337@gmail.com>
2024-06-01 15:58:30 +0530

    configured the gomod file for linux

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ func main() {
 	var nigga_name string
 	var niggas_details [5]string
 
-	fmt.Print("Please Enter your name:")
+	fmt.Print("Please Enter your name: ")
 	fmt.Scan(&CustomerName)
 	fmt.Printf("Hey %v Welcome to Nigga Trade Centre\n", CustomerName)
-	fmt.Printf("We have Total: %v \nAnd Remaining: %v \nniggers to Book in the Basement\n", TotalAllowedNiggas, remainingNiggas)
+	fmt.Printf("We have Total: %v Niggers\nAnd Remaining: %v Niggers", TotalAllowedNiggas, remainingNiggas)
 	fmt.Print("Please Enter the number of Niggas to Book:")
 	fmt.Scan(&SellingNiggaCount)
 	fmt.Printf("%v Booked %v Niggers for their Basement\n", CustomerName, SellingNiggaCount)
@@ -23,6 +23,5 @@ func main() {
 	fmt.Scan(&nigga_name)
 	niggas_details[0] = nigga_name
 	remainingNiggas = remainingNiggas - SellingNiggaCount
-	fmt.Printf("Remaining Niggas are : %v", remainingNiggas)
-
+	fmt.Printf("Remaining Niggas are : %v\n", remainingNiggas)
 }
